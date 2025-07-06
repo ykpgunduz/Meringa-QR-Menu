1eee6d30ccbe18ce1f16c2b049ec2433074827ee
yakupgit <137324492+ykpgunduz@users.noreply.github.com>
2025-07-06 14:42:25 +0300

    edited

--- routes/web.php
+++ routes/web.php
@@ -10,10 +10,6 @@ Route::get('/categories', function () {
     return view('category');
 })->name('categories');
 
-Route::get('/products', function () {
-    return view('product');
-})->name('products');
-
 Route::get('/category/{key}', function () {
     return view('category_products');
 });
